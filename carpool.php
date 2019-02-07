c285a0b519394c9505dbc24cde2af8defc9587d9
pavaniboosa <boosapi@mail.uc.edu>
2019-02-07 15:36:07 -0500

    this is the carpool first page

---
+++ carpool.php
@@ -0,0 +1,44 @@
+
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<style>
+* {
+        box-sizing: border-box;
+}
+.menuitem {
+        padding: 8px;
+        margin-top: 7px;
+        border-bottom: 1px solid #f1f1f1;
+}
+.button1{
+        float: right;
+        width: 90%;
+        padding: 0 48px;
+        overflow: hidden;
+        }
+        .button1{
+        float: left;
+        width: 40%;
+        padding: 0 48px;
+        overflow: hidden;
+        }
+*/
+</style>
+</head>
+<body style="font-family:Verdana;" background="http://nebula.wsimg.com/0a1ba28df8532ff68cceea23a43c844b?AccessKeyId=5DFBC2474A2ABBE62EE7&disposition=0&allow$
+<div style="background-color:#3A3A3A;padding:15px;">
+        <h1 style="color:blue";font size="10">CAR POOL</h1>
+        <h3 style="color:red;">Make your own ride with comfort</h3>
+<div class="button1">
+<button type="button"onclick="document.location='http://ec2-18-222-60-113.us-east-2.compute.amazonaws.com/in.php'" />I DRIVE.</button>
+</div>
+<div class="button2">
+<button type="button" onclick="document.location='http://ec2-18-222-60-113.us-east-2.compute.amazonaws.com/in.php'" >I RIDE.</button>
+</div>
+</div>
+
+</body>
+</html>
+
