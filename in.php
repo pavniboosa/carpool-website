518302d3ee07819bc74c7f72bc253d1675eb67be
pavaniboosa <boosapi@mail.uc.edu>
2019-02-07 15:36:55 -0500

    this is the login page with your first and last name

---
+++ in.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <style>
+        input[type=text], input[type=password] {
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  box-sizing: border-box;
+}
+button {
+  background-color: #4CAF50;
+  color: white;
+  padding: 14px 10px;
+  margin: 8px 0;
+  border: none;
+  cursor: pointer;
+  width: 20%;
+}
+
+        .container {
+  padding: 10px;
+}
+        <title>Section 2: JavaScript Language Basics</title>
+    </head>
+</style>
+    <body>
+        <div class="center">
+        <center style="color:blue"><font size="16">LOGIN PAGE</center>
+        </div>
+   <center><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRt_zdfxn6l1zaULEdPPx_F1zZNAb1JFdJzqeqqwXOWxlFXWpbf" alt="loginimage" width="150" $
+   </center>
+        <div class="container">
+ <label for="uname"><b>firstname</b></label>
+      <input type="text" placeholder="Enter firstname" name="uname" required>
+      <label for="psw"><b>lastname</b></label>
+<input type="password" placeholder="Enter lastname" name="psw" required>
+<div>
+       <button type="submit" onclick="document.location='http://ec2-18-222-60-113.us-east-2.compute.amazonaws.com/tripp.php'" / >Login</button>
+
+</div>
+      <label>
+        <input type="checkbox" checked="checked" name="remember"> Remember me
+      </label>
+  </div>
+
+    </body>
+
+    <script src="spread.js"> </script>
+</html>
